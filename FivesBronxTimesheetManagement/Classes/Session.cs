c9082decb3f46b1a5a76d047d31565cb447f6a93
17robots <mdray@ameritech.net>
2020-10-07 00:33:28 -0400

    Fix #68

--- FivesBronxTimesheetManagement/Classes/Session.cs
+++ FivesBronxTimesheetManagement/Classes/Session.cs
@@ -1,13 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Windows;
 
 namespace FivesBronxTimesheetManagement.Classes
 {
-    class Session
+    static class Session
     {
+        // deletes the session file if it exists
+        public static void Logout()
+        {
+            string path = string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "\\session.txt");
+            if (File.Exists(path)) File.Delete(path);
+        }
 
+        // writes the values to the session file but if they already exist then it 
+        public static void CreateSession(string user, string password)
+        {
+            string path = string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "\\session.txt");
+
+            TextWriter writer = File.CreateText(path);
+            writer.WriteLine(user);
+            writer.WriteLine(password);
+            writer.Close();
+        }
+
+        // tries to read the values from the file and returns if it could or not
+        public static bool ReadSession(ref string username, ref string password)
+        {
+            StreamReader reader;
+            string path;
+            try
+            {
+                path = string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "\\session.txt");
+                if (File.Exists(path))
+                {
+                    reader = new StreamReader(path);
+                    try
+                    {
+                        username = reader.ReadLine();
+                        password = reader.ReadLine();
+                    }
+                    finally
+                    {
+                        ((IDisposable)reader).Dispose();
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return false;
+            }
+        }
     }
 }
--- Forms/UserLogin.xaml.cs
+++ Forms/UserLogin.xaml.cs
@@ -1,5 +1,6 @@
 using FivesBronxTimesheetManagement.Classes;
 using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.Logging;
 using MySql.Data.MySqlClient;
 using System;
 using System.CodeDom.Compiler;
@@ -16,17 +17,21 @@ namespace FivesBronxTimesheetManagement.Forms
 	{
 		private Connection myConnection = new Connection();
 
-		//private Queries queries = new Queries();
 		private Queries2 queries = new Queries2();
 
 		public UserLogin()
 		{
-			this.InitializeComponent();
+			InitializeComponent();
+			string user = "", pass = "";
+			if(Session.ReadSession(ref user, ref pass))
+			{
+				Login(user, pass);
+			}
 		}
 
 		private void btnLogin_Click(object sender, RoutedEventArgs e)
 		{
-			this.Login();
+			Login();
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
@@ -50,6 +55,7 @@ namespace FivesBronxTimesheetManagement.Forms
 				if (num == 1)
 				{
 					this.myConnection.Close();
+					Session.CreateSession(txtEmployeeId.Text, txtPassword.Password);
 					MainWindow mainWindow = new MainWindow(this.queries.GetUser(int.Parse(this.txtEmployeeId.Text)));
 					mainWindow.Show();
 					base.Close();
@@ -62,6 +68,43 @@ namespace FivesBronxTimesheetManagement.Forms
 				else
 				{
 					MessageBox.Show("Duplicate Username and password.... Access Denied");
+					this.myConnection.Close();
+				}
+			}
+			catch (Exception exception)
+			{
+				MessageBox.Show(exception.Message);
+			}
+		}
+
+		private void Login(string user, string pass)
+		{
+			try
+			{
+				string[] text = new string[] { "SELECT * FROM login WHERE user_id='", user, "' AND password='", pass, "'" };
+				MySqlCommand mySqlCommand = new MySqlCommand(string.Concat(text), myConnection.MySqlConnection);
+				myConnection.Open();
+				MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+				int num = 0;
+				while (mySqlDataReader.Read())
+				{
+					num++;
+				}
+				if (num == 1)
+				{
+					myConnection.Close();
+					MainWindow mainWindow = new MainWindow(queries.GetUser(int.Parse(user)));
+					mainWindow.Show();
+					Close();
+				}
+				else if (num <= 1)
+				{
+					MessageBox.Show("Incorrect Username and/or Password");
+					this.myConnection.Close();
+				}
+				else
+				{
+					MessageBox.Show("Duplicate Username and password.... Access Denied. Please Speak To Administrator Of Database.");
 					this.myConnection.Close();
 				}
 			}
