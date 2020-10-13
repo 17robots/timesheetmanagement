661caeea474897625080a9c5ec8ae4447c09997a
17robots <mdray@ameritech.net>
2020-10-13 00:47:49 -0400

    Fix #85

--- Forms/About.xaml.cs
+++ Forms/About.xaml.cs
@@ -1,3 +1,4 @@
+using FivesBronxTimesheetManagement.Classes;
 using System.Reflection;
 using System.Windows;
 
@@ -5,10 +6,17 @@ namespace FivesBronxTimesheetManagement.Forms
 {
 	public partial class About : Window
 	{
-		public About()
+		private User user;
+		public About(User user)
 		{
 			InitializeComponent();
 			lblVersion.Content = string.Concat("    ", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+			this.user = user;
+		}
+
+		private void btnFileChangePassword_Click(object sender, RoutedEventArgs e)
+		{
+			new UserChangePassword(user).Show();
 		}
 	}
 }
