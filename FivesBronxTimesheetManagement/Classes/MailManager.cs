a7cf2f982b246ede2188b02e1427de58874badbd
17robots <mdray@ameritech.net>
2021-05-30 15:05:27 -0400

    Moving to new workspace

---
+++ FivesBronxTimesheetManagement/Classes/MailManager.cs
@@ -0,0 +1,24 @@
+﻿using Aspose.Email;
+
+namespace FivesBronxTimesheetManagement.Classes
+{
+    class MailManager
+    {
+        private MailMessage message;
+
+        public MailManager(string to, string subject)
+        {
+            message.To.Add(new MailAddress("", "", false));
+        }
+
+        public static bool sendRejection()
+        {
+            return true;
+        }
+
+        public static bool sendSubmission()
+        {
+            return true;
+        }
+    }
+}
